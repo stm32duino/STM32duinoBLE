ea2c423e8a99daac3a07db4140eed44d020c18dc
Carlo Parata <carlo.parata@st.com>
2020-02-07 11:20:49 +0100

    chore(GAP): improve scan
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- src/local/BLELocalDevice.cpp
+++ src/local/BLELocalDevice.cpp
@@ -426,9 +426,9 @@ int BLELocalDevice::scanForAddress(String address, bool withDuplicates)
   return GAP.scanForAddress(address, withDuplicates);
 }
 
-void BLELocalDevice::stopScan()
+int BLELocalDevice::stopScan()
 {
-  GAP.stopScan();
+  return GAP.stopScan();
 }
 
 BLEDevice BLELocalDevice::central()
